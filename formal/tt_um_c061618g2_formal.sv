1820bac4caa285c6692118e56f1f81709e671649
circuitli <dev@circuili.ch>
2026-08-14 17:33:51 +0200

    Remove argument

--- formal/tt_um_c061618g2_formal.sv
+++ formal/tt_um_c061618g2_formal.sv
@@ -43,7 +43,6 @@ module tt_um_c061618g2_formal (
         .uo_out  (uo_out),
         .uio_in  (uio_in),
         .uio_out (uio_out),
-        .uio_oe  (uio_oe),
         .ena     (ena),
         .clk     (clk),
         .rst_n   (rst_n)
