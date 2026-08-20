0c7529e7a47dcafdd4ded4b10c7ad5539f1e27ca
circuitli <dev@circuili.ch>
2026-08-20 15:17:10 +0200

    Crash

--- src/floorplan_hooks.tcl
+++ src/floorplan_hooks.tcl
@@ -1,1 +1,4 @@
+# Force an intentional crash to test loading
+THIS_IS_A_FAKE_COMMAND_TO_CRASH_THE_FLOW
+
 add_to_specification_region -name u_c061618g2_region -box {10.0 10.0 55.36 25.12} -instances [get_cells -hierarchical *u_c061618g2]
