6f051b3d94ad0e9a0da27df5b1840b5080d9185b
circuitli <dev@circuili.ch>
2026-08-20 17:59:05 +0200

    Repair

--- src/tt_um_c061618g2.sv
+++ src/tt_um_c061618g2.sv
@@ -32,10 +32,7 @@ module tt_um_c061618g2 (
     input  [0:0] rst_n     // Part of the strict wrapper standard!
 );
 
-    (* keep_hierarchy = "TRUE" *) 
-    (* blackbox *)
-    (* box_width = 45.36 *)
-    (* box_height = 15.12 *)   
+    (* keep_hierarchy = "TRUE" *)   
     c061618g2 u_c061618g2 (
         .ui_in(ui_in),    
         .uo_out(uo_out),  
