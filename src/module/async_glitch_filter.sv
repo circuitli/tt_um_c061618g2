d57e70f349e6fd908d51113c63f17162ae659c80
circuitli <dev@circuili.ch>
2026-08-25 04:52:36 +0200

    Control optimizer

--- src/module/async_glitch_filter.sv
+++ src/module/async_glitch_filter.sv
@@ -29,7 +29,7 @@ module async_glitch_filter #(
 );
 
     // Packed variable split to prevent flat evaluation dependencies
-    (* keep = 1 *) wire [STAGES-1:0] delay_chain /*verilator split_var*/;
+    (* dont_touch = "true" *) wire [STAGES:0] delay_chain;
 
     // =========================================================================
     // CASCADED ASYNCHRONOUS BUFFER TREE (ONE-WAY BALANCED TIMELINE)
