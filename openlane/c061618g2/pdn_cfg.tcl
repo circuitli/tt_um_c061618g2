b6f0ac5085056dfe2f62efe45343423dde6ba4a2
circuitli <dev@circuili.ch>
2026-09-08 21:53:54 +0200

    Arg fix

--- openlane/c061618g2/pdn_cfg.tcl
+++ openlane/c061618g2/pdn_cfg.tcl
@@ -25,9 +25,10 @@ foreach vdd $::env(VDD_NETS) gnd $::env(GND_NETS) {
 
 set_voltage_domain -name CORE -power $::env(VDD_NET) -ground $::env(GND_NET) -secondary_power $secondary
 
-define_pdn_grid -name stdcell_grid -starts_with POWER -voltage_domain CORE -pins "Metal3"
+# Fixed: Removed the invalid -pins token completely from the standard cell declaration
+define_pdn_grid -name stdcell_grid -starts_with POWER -voltage_domains CORE
 
-add_pdn_stripe -grid stdcell_grid -layer Metal3 -width $::env(PDN_VWIDTH) -pitch $::env(PDN_VPITCH) -offset $::env(PDN_VOFFSET) -spacing $::env(PDN_VSPACING) -starts_with POWER -extend_to boundary
+add_pdn_stripe -grid stdcell_grid -layer Metal3 -width $::env(PDN_VWIDTH) -pitch $::env(PDN_VPITCH) -offset $::env(PDN_VOFFSET) -spacing $::env(PDN_VSPACING) -starts_with POWER -extend_to_boundary
 
 if { $::env(PDN_ENABLE_RAILS) == 1 } {
     add_pdn_stripe -grid stdcell_grid -layer Metal1 -followpins
@@ -36,4 +37,4 @@ if { $::env(PDN_ENABLE_RAILS) == 1 } {
 
 define_pdn_grid -macro -default -name macro_grid -starts_with POWER
 
-add_pdn_connect -grid macro_grid -layers "Metal3 Metal4"
+add_pdn_connect -grid macro_grid -layers "Metal3 Metal4"l3 Metal4"
