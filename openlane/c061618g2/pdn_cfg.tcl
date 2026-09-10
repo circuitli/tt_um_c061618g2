7ccdadd4bbb96c862b489d6f6eb83d0586476b69
circuitli <dev@circuili.ch>
2026-09-10 05:53:17 +0200

    Add extend_to_core_ring

--- openlane/c061618g2/pdn_cfg.tcl
+++ openlane/c061618g2/pdn_cfg.tcl
@@ -46,7 +46,8 @@ add_pdn_stripe -grid stdcell_grid \
                -layer $::env(PDN_RAIL_LAYER) \
                -width $native_pdk_width \
                -followpins \
-               -nets "$::env(GND_NET) $::env(VDD_NET)"
+               -nets "$::env(GND_NET) $::env(VDD_NET)" \
+               -extend_to_core_ring
 
 # 3. Horizontal Mesh Power Landing Pads (Metal4) -> EXTEND_TO_BOUNDARY
 add_pdn_stripe -grid stdcell_grid \
