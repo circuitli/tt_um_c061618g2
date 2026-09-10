957ac39d1fe2dff6f4cda0d94e916207c75ad9d6
circuitli <dev@circuili.ch>
2026-09-10 23:50:05 +0200

    project.sv

--- formal/project_formal.sv
+++ formal/project_formal.sv
@@ -20,7 +20,7 @@
 /// ==============================================================================
 // SECTION: TOP-LEVEL HARDWARE WRAPPER FORMAL PROPERTIES
 // ==============================================================================
-`include "src/project.v"
+`include "src/project.sv"
 
 `include "formal/module/c061618g2_formal.sv"
 
