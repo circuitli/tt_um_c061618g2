42e351f30d6d174c6bd11f8516e01cf97e753f74
circuitli <dev@circuili.ch>
2026-09-10 06:05:11 +0200

    Remove stripe

--- openlane/c061618g2/pdn_cfg.tcl
+++ openlane/c061618g2/pdn_cfg.tcl
@@ -41,14 +41,6 @@ add_pdn_stripe -grid stdcell_grid \
                -nets "$::env(GND_NET) $::env(VDD_NET)" \
                -extend_to_boundary
 
-# 2. Horizontal Power Rails (Metal1) 
-add_pdn_stripe -grid stdcell_grid \
-               -layer $::env(PDN_RAIL_LAYER) \
-               -width $native_pdk_width \
-               -followpins \
-               -nets "$::env(GND_NET) $::env(VDD_NET)" \
-               -extend_to_core
-
 # 3. Horizontal Mesh Power Landing Pads (Metal4) -> EXTEND_TO_BOUNDARY
 add_pdn_stripe -grid stdcell_grid \
                -layer $::env(PDN_HORIZONTAL_LAYER) \
