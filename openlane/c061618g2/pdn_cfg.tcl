b53425bd08fd01d8a641b6057603c393ac49cabb
circuitli <dev@circuili.ch>
2026-09-12 02:40:07 +0200

    No extension

--- openlane/c061618g2/pdn_cfg.tcl
+++ openlane/c061618g2/pdn_cfg.tcl
@@ -46,8 +46,7 @@ add_pdn_stripe -grid stdcell_grid \
                -layer $::env(PDN_RAIL_LAYER) \
                -width $native_pdk_width \
                -followpins \
-               -nets "$::env(GND_NET) $::env(VDD_NET)" \
-               -extend_to_boundary
+               -nets "$::env(GND_NET) $::env(VDD_NET)" 
 
 # 3. Horizontal Mesh Power Landing Pads (Metal4) -> EXTEND_TO_BOUNDARY
 add_pdn_stripe -grid stdcell_grid \
